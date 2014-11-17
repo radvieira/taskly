31fc4325f5d969047f2272963253a1df054a401e
Raul Vieira <radvieira@gmail.com>
2014-11-16 21:05:49 -0500

    serving from connect server

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+
+var grunt = require('grunt');
+
+grunt.initConfig({
+  connect: {
+    'default': {
+      port: 9000,
+      base: './public'
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-connect');
+
+grunt.registerTask('default', 'connect');
