d9224cf0871579365687e3626909ea4991464835
Raul Vieira <radvieira@gmail.com>
2014-11-17 10:49:13 -0500

    serving content with connect middleware

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,33 @@ var grunt = require('grunt');
 
 grunt.initConfig({
   connect: {
-    'default': {
-      port: 9000,
-      base: './public'
+    server: {
+      options: {
+        port: 9000,
+        base: 'public',
+        keepalive: true,
+        middleware: function(connect, options) {
+          return [
+            connect.static(options.base[0]),
+            function(req, res, next) {
+
+              if (req.url === '/api/tasks') {
+
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify([
+                  { name: 'Get some mile' }
+                ]));
+                return res.end();
+              }
+
+              return next();
+            }
+          ]
+        }
+      }
+
     }
   }
 });
 
-grunt.loadNpmTasks('grunt-connect');
-
-grunt.registerTask('serve', 'connect');
+grunt.loadNpmTasks('grunt-contrib-connect');
